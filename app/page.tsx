5096d867e773034815034f15f1d4889a73e332ad
Wilker <wilker.soh@cognizant.com>
2023-08-31 15:48:50 +0800

    change to app routes

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "My Page Title",
+};
+
+const RootIndex = () => {
+    return <div>page</div>;
+};
+
+export default RootIndex;
